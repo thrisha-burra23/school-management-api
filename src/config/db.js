5a6dc530522e87184e5fd441b79f89c91ebfe2f4
Thrisha Burra <thrishagoud023@gmail.com>
2026-03-25 23:37:06 +0530

    fix duplicate export error

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2"
 
 dotenv.config();
 
-export const db = mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
